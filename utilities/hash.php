ef3e570dcca86c284cf9fa0ec30dd03f53d52577
jestoc01 <jes@twc.com>
2016-01-31 19:03:43 -0500

    Add utility to create stronger passwords

---
+++ utilities/hash.php
@@ -0,0 +1,10 @@
+
+
+<?php
+echo "Enter e-mail:\n";
+$email=trim(fgets(STDIN));
+echo "Enter password:\n";
+$password=trim(fgets(STDIN));
+echo "Your hash is:\n";
+echo password_hash($email . $password, PASSWORD_DEFAULT);
+?>
